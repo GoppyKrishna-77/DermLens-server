4afd4f474b109ad7ae3ea3f7939ef12e4c7bdbef
Goppy Krishna <83293163+GoppyKrishna-77@users.noreply.github.com>
2024-03-10 17:58:56 +0530

    Add files via upload

--- api/app.py
+++ api/app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
   return "Hello World!"
+
 @app.route("/about")
-def hello():
+def helloToo():
   return "Hello this is about page!, I'm running using Vercel"
 
 if __name__ == "__main__":
